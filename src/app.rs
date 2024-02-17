ecf3783b3763e33244e83a6d538fe4543aa92614
AndersLH <andelha@stud.ntnu.no>
2024-02-17 18:16:24 +0100

    Basic implementation of infinite feed that feeds the webpage one image at a time as you scroll down

--- src/app.rs
+++ src/app.rs
@@ -56,14 +56,8 @@ fn PictureDiv() -> impl IntoView {
 #[component]
 fn HomePage() -> impl IntoView {
 
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
         <h1>"Home"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-        <br/>
         // <DynamicList initial_length=5 initial_period=1/>
         <InfiniteFeed/>
     }
@@ -75,7 +69,8 @@ struct Image {
     date: String,
 }
 
-fn fetch_image() -> Vec<(i32,Vec<(i32,Vec<(i32,Vec<Image>)>)>)> {
+fn fetch_image(num: usize) -> Option<Image> {
+// fn fetch_image() -> Vec<(i32,Vec<(i32,Vec<(i32,Vec<Image>)>)>)> {
 
     //TEST DATA
     //======================================================
@@ -89,19 +84,19 @@ fn fetch_image() -> Vec<(i32,Vec<(i32,Vec<(i32,Vec<Image>)>)>)> {
             date: "2024-01-15".to_string(),
         },
         Image {
-            src: "pic1.jpg".to_string(),
+            src: "pic11.jpg".to_string(),
             date: "2024-01-15".to_string(),
         },
         Image {
-            src: "pic1.jpg".to_string(),
+            src: "pic111.jpg".to_string(),
             date: "2024-01-15".to_string(),
         },
         Image {
-            src: "pic1.jpg".to_string(),
+            src: "pic1111.jpg".to_string(),
             date: "2024-01-15".to_string(),
         },
         Image {
-            src: "pic1.jpg".to_string(),
+            src: "pic11111.jpg".to_string(),
             date: "2024-01-15".to_string(),
         },
         Image {
@@ -113,71 +108,71 @@ fn fetch_image() -> Vec<(i32,Vec<(i32,Vec<(i32,Vec<Image>)>)>)> {
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic33.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic333.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic3333.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic33333.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic31.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic313.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic31313.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic32.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic323.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic3232.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic322223.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic321233.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic333133333.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic30.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic39.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic388.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
-            src: "pic3.jpg".to_string(),
+            src: "pic354.jpg".to_string(),
             date: "2024-01-10".to_string(),
         },
         Image {
@@ -215,114 +210,136 @@ fn fetch_image() -> Vec<(i32,Vec<(i32,Vec<(i32,Vec<Image>)>)>)> {
     //Sort images by date (yyyy-mm-dd)
     images.sort_by(|a, b| a.date.cmp(&b.date));
 
-    //Vec<(Year, Vec<(Month, Vec<Day, Vec<Image>)>)>)>
-    let mut sorted_images: Vec<(i32, Vec<(i32, Vec<(i32, Vec<Image>)>)>)> = Vec::new();
-
-    //Iterate the list and sort by year, then month, then day
-    for image in &images {
-        //Parse dates to i32
-        let year = image.date[..4].parse::<i32>().unwrap();
-        let month = image.date[5..7].parse::<i32>().unwrap();
-        let day = image.date[8..10].parse::<i32>().unwrap();
-
-        //Check if the year, month or day exists, to ensure there is no repetition
-        if let Some((_, months)) = sorted_images.iter_mut().find(|(y, _)| *y == year) {
-            if let Some((_, days)) = months.iter_mut().find(|(m, _)| *m == month) {
-                if let Some((_, images_for_day)) = days.iter_mut().find(|(d, _)| *d == day) {
-                    images_for_day.push(image.clone());
-                } else {
-                    days.push((day, vec![image.clone()]));
-                    days.sort_by_key(|(d, _)| *d);
-                }
-            } else {
-                months.push((month, vec![(day, vec![image.clone()])]));
-                months.sort_by_key(|(m, _)| *m);
-            }
-        } else {
-            sorted_images.push((year, vec![(month, vec![(day, vec![image.clone()])])]));
-            sorted_images.sort_by_key(|(y, _)| *y);
-        }
+    //Check if valid parameter
+    if num < images.len() {
+        Some(images[num].clone())
+    } else {
+        //If out of bounds
+        None
     }
+
+
+    // //Vec<(Year, Vec<(Month, Vec<Day, Vec<Image>)>)>)>
+    // let mut sorted_images: Vec<(i32, Vec<(i32, Vec<(i32, Vec<Image>)>)>)> = Vec::new();
+
+    // //Iterate the list and sort by year, then month, then day
+    // for image in &images {
+    //     //Parse dates to i32
+    //     let year = image.date[..4].parse::<i32>().unwrap();
+    //     let month = image.date[5..7].parse::<i32>().unwrap();
+    //     let day = image.date[8..10].parse::<i32>().unwrap();
+
+    //     //Check if the year, month or day exists, to ensure there is no repetition
+    //     if let Some((_, months)) = sorted_images.iter_mut().find(|(y, _)| *y == year) {
+    //         if let Some((_, days)) = months.iter_mut().find(|(m, _)| *m == month) {
+    //             if let Some((_, images_for_day)) = days.iter_mut().find(|(d, _)| *d == day) {
+    //                 images_for_day.push(image.clone());
+    //             } else {
+    //                 days.push((day, vec![image.clone()]));
+    //                 days.sort_by_key(|(d, _)| *d);
+    //             }
+    //         } else {
+    //             months.push((month, vec![(day, vec![image.clone()])]));
+    //             months.sort_by_key(|(m, _)| *m);
+    //         }
+    //     } else {
+    //         sorted_images.push((year, vec![(month, vec![(day, vec![image.clone()])])]));
+    //         sorted_images.sort_by_key(|(y, _)| *y);
+    //     }
+    // }
 
     //Not needed, but keeping it beacuse Im scared to delete it :(
     // sorted_images.sort_by_key(|(y, _)| *y);
         
-    sorted_images
+    // sorted_images
 }
+
 
 #[component]
 fn infinite_feed() -> impl IntoView {
-
-    let (photos, _set_photos) = create_signal(fetch_image());
-    
     let el = create_node_ref::<Div>();
 
-    //old
-    // let (data, set_data) = create_signal(vec![1, 2, 3, 4, 5, 6]);
-
+    let mut next_image_number = 0;
+    let mut image_vector = Vec::new();  
+    image_vector.push(fetch_image(next_image_number));
+    let (feed, set_feed) = create_signal(image_vector);
+    
     let _ = use_infinite_scroll_with_options(
         el,
         move |_| async move {
-            //old
-            // let len = data.with_untracked(|d| d.len());
-            // set_data.update(|data| *data = (1..len + 6).collect());
+            //Get len of vec
+            let len = feed.with_untracked(|d| d.len());
+            //Check for next image and push to feed
+            if let Some(next_image) = fetch_image(len+1) {
+                set_feed.update(|feed| feed.push(Some(next_image)));
+            }
         },
         UseInfiniteScrollOptions::default().distance(10.0),
     );
 
     view!{
         //Year
-        <div>
+        <div class="flowdiv" node_ref=el>
         <For 
-          each=photos 
-          key=|yearkey| yearkey.clone() 
-          let:year>
-            <br/>
-            <br/>
-            {year.0}
-            <br/>
-            //Month
-            <For 
-              each=move||{year.1.clone()}
-              key=|monthkey| monthkey.clone()
-              let:month>
-                <br/>
-                {match month.0 {
-                    1 => "January",
-                    2 => "February",
-                    3 => "March",
-                    4 => "April",
-                    5 => "May",
-                    6 => "June",
-                    7 => "July",
-                    8 => "August",
-                    9 => "September",
-                    10 => "October",
-                    11 => "November",
-                    12 => "December",
-                    _ => ""
-                }}
-                <br/>
-                //Day
-                <For 
-                  each=move||{month.1.clone()}
-                  key=|daykey| daykey.clone()
-                  let:day>
-                    // --{day.0} 
-                    //Vec of images in one day
-                    <For 
-                      each=move||{day.1.clone()}
-                      key=|imgkey| imgkey.clone()
-                      let:img>
-                        <img src={img.src} 
-                        style:width=rand::thread_rng().gen_range(150..350).to_string()+"px"
-                        style:height=rand::thread_rng().gen_range(150..350).to_string()+"px"
-                        />
-                    </For>
-                </For>
-            </For>
+          each=feed
+          key=|photokey| photokey.clone() 
+          let:block>
+          {block.unwrap().src}
+          <img src="sss" 
+                style:height=rand::thread_rng().gen_range(250..350).to_string()+"px"
+                style:width=rand::thread_rng().gen_range(250..350).to_string()+"px"
+                />
         </For>
         </div>
+        // <For 
+        //   each=photos 
+        //   key=|yearkey| yearkey.clone() 
+        //   let:year>
+        //     <br/>
+        //     <br/>
+        //     {year.0}
+        //     <br/>
+        //     //Month
+        //     <For 
+        //       each=move||{year.1.clone()}
+        //       key=|monthkey| monthkey.clone()
+        //       let:month>
+        //         <br/>
+        //         {match month.0 {
+        //             1 => "January",
+        //             2 => "February",
+        //             3 => "March",
+        //             4 => "April",
+        //             5 => "May",
+        //             6 => "June",
+        //             7 => "July",
+        //             8 => "August",
+        //             9 => "September",
+        //             10 => "October",
+        //             11 => "November",
+        //             12 => "December",
+        //             _ => ""
+        //         }}
+        //         <br/>
+        //         //Day
+        //         <For 
+        //           each=move||{month.1.clone()}
+        //           key=|daykey| daykey.clone()
+        //           let:day>
+        //             // --{day.0} 
+        //             //Vec of images in one day
+        //             <For 
+        //               each=move||{day.1.clone()}
+        //               key=|imgkey| imgkey.clone()
+        //               let:img>
+        //                 <img src={img.src} 
+        //                 style:width=rand::thread_rng().gen_range(150..350).to_string()+"px"
+        //                 style:height=rand::thread_rng().gen_range(150..350).to_string()+"px"
+        //                 />
+        //             </For>
+        //         </For>
+        //     </For>
+        // </For>
 
 
         
@@ -334,7 +351,7 @@ fn infinite_feed() -> impl IntoView {
 fn TestPage() -> impl IntoView {
     let el = create_node_ref::<Div>();
 
-    let (data, set_data) = create_signal(vec![1, 2, 3, 4, 5, 6]);
+    let (data, set_data) = create_signal(vec![1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]);
 
     let _ = use_infinite_scroll_with_options(
         el,
