85d9f430f3aa61a0f0ef237358cf7e582020a1fc
krs <kristianrorensvanholm@gmail.com>
2024-02-18 18:21:34 +0100

    First server function interacting with db

--- src/app.rs
+++ src/app.rs
@@ -38,6 +38,61 @@ pub fn App() -> impl IntoView {
     }
 }
 
+
+#[derive(Debug, leptos::server_fn::serde::Serialize, leptos::server_fn::serde::Deserialize)]
+pub struct Folder {
+    id: i32,
+    parent_id: Option<i32>,
+    name: String,
+} 
+
+#[cfg(feature = "ssr")]
+pub mod db;
+
+#[server(TestDB, "/api")]
+pub async fn test_db() -> Result<Vec<Folder>, ServerFnError> {
+    let conn = crate::app::db::db().await?;
+
+    /* // Insert & parameters example. Uncomment to add to DB.
+    use rusqlite::params;
+    let _ = conn.execute(
+            "INSERT INTO folder (name) values (?1)",
+            params!["hello"],
+        )?; 
+    */
+
+    let mut stmnt = conn.prepare("SELECT id, parentId, name FROM folder")?;
+
+    let folders = stmnt.query_map([], |row| {
+        Ok(Folder {
+            id: row.get(0)?,
+            parent_id: row.get(1)?,
+            name: row.get(2)?,
+        })
+    })?;
+
+    
+    let mut vec = Vec::new();
+    for folder in folders {
+        vec.push(folder.unwrap());
+    }
+
+    Ok(vec)
+}
+
+#[component]
+pub fn TestDBButton() -> impl IntoView {
+    view! {
+        <button on:click=move |_| {
+            spawn_local(async {
+                logging::log!("{:?}",test_db().await.unwrap());
+            });
+        }>
+            "Add Todo"
+        </button>
+    }
+}
+
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
@@ -46,7 +101,9 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
+        <TestDBButton></TestDBButton>
         <h1>"Home"</h1>
+        <h1>HELLO WORLD</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
     }
 }
