6c6c3add8ae7cdbb95a5edb68d518943a95c0027
Kristian Røren Svanholm <61845965+KristianSvanholm@users.noreply.github.com>
2024-02-19 20:05:48 +0100

    Feature/iip 5 db init (#2)
    * Initial db schema and db template
    
    * new dependencies
    
    * Not working db init
    
    * db sql
    
    * sqlite sql db
    
    * Working DB initiatlization
    
    * First server function interacting with db
    
    * Ignore db file
    
    * delete old duckdb template
    
    * Add vscode fix for SSR main grayed out issue
    
    * Application now available on local network
    
    * Take string input for db test and cut list at 10 newest elements
    
    * DB now uses UUID
    
    * New function for auto creating DB and updated sql file

--- src/app.rs
+++ src/app.rs
@@ -38,6 +38,74 @@ pub fn App() -> impl IntoView {
     }
 }
 
+#[derive(
+    Debug, Clone, leptos::server_fn::serde::Serialize, leptos::server_fn::serde::Deserialize,
+)]
+pub struct Folder {
+    id: String,
+    parent_id: Option<String>,
+    name: String,
+}
+
+#[cfg(feature = "ssr")]
+pub mod db;
+
+#[server(TestDB, "/api")]
+pub async fn test_db(name: String) -> Result<Vec<Folder>, ServerFnError> {
+    use uuid::Uuid;
+
+    // Connect to db
+    let conn = crate::app::db::db().await?;
+
+    // Insert & parameters example. Uncomment to add to DB.
+    use rusqlite::params;
+    let _ = conn.execute("INSERT INTO folder (id, name, createdDate) values (?1, ?2, ?3)", 
+        params![Uuid::new_v4().to_string(),name, "now:)"])?;
+
+    let mut stmnt = conn.prepare("SELECT id, parentId, name FROM folder")?;
+
+    let folders = stmnt.query_map([], |row| {
+        Ok(Folder {
+            id: row.get(0)?,
+            parent_id: row.get(1)?,
+            name: row.get(2)?,
+        })
+    })?;
+
+    let mut vec = Vec::new();
+    for folder in folders {
+        vec.push(folder.unwrap());
+    }
+
+    use std::cmp;
+    Ok(vec[cmp::max(vec.len() - cmp::min(vec.len(), 10), 0)..].to_vec())
+}
+
+#[component]
+pub fn TestDBButton() -> impl IntoView {
+    let (name, _set_name) = create_signal("Controlled".to_string());
+    let input_el: NodeRef<html::Input> = create_node_ref();
+
+    let on_submit = move |ev: leptos::ev::SubmitEvent| {
+        ev.prevent_default();
+
+        let value = input_el().expect("<Input> should be mounted").value();
+        spawn_local(async {
+            logging::log!("{:?}", test_db(value).await.unwrap());
+        });
+    };
+
+    view! {
+        <form on:submit=on_submit>
+            <input type="text"
+                value=name
+                node_ref=input_el
+            />
+            <input type="submit" value="Submit"/>
+        </form>
+    }
+}
+
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
@@ -46,7 +114,9 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
+        <TestDBButton></TestDBButton>
         <h1>"Home"</h1>
+        <h1>HELLO WORLD</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
     }
 }
@@ -67,7 +137,6 @@ fn HelloPage() -> impl IntoView {
 
 #[component]
 fn TopBar() -> impl IntoView {
-
     // All routes accessible from navigation bar
     view! {
         <nav>
