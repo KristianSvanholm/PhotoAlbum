e7dfdaf73a094ff63f93263fb8577963f592bbfd
Anders Lunde Hagen <143401892+AndersLH@users.noreply.github.com>
2024-02-20 11:17:48 +0100

    Feature/iip 7 infinite feed (#3)
    * Semi-random divs with row layout
    
    * Added a date feature to seperate dates for testing in the feed
    
    * Minor style changes for flex-grow
    
    * Attempt at leptos_use (panics)
    
    * Changed the sorting of images
    Images sorted by year, month and day dynamically
    
    * Removed non-functinoning infinite scroll test code
    
    * Basic implementation of infinite feed that feeds the webpage one image at a time as you scroll down
    
    * load multiple images each scroll
    
    * Changed scrollbar style
    
    * Reduced image count to 20 to showcase infinite feed more
    
    * No rounded corners on one side
    
    * Changed paramter for infinite feed load
    
    * Removed duplicate rand
    
    ---------
    
    Co-authored-by: AndersLH <andelha@stud.ntnu.no>
    Co-authored-by: Ivan Norderhaug <norderhaug@protonmail.com>

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,12 @@ use crate::error_template::{AppError, ErrorTemplate};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+extern crate rand; 
+use rand::Rng;
+use leptos::html::Div;
+use leptos_use::{UseInfiniteScrollOptions, use_infinite_scroll_with_options};
+
+
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -109,29 +115,125 @@ pub fn TestDBButton() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
         <TestDBButton></TestDBButton>
         <h1>"Home"</h1>
-        <h1>HELLO WORLD</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        // <DynamicList initial_length=5 initial_period=1/>
+        <InfiniteFeed/>
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Image {
+    src: String,
+    date: String,
+}
+
+const FETCH_IMAGE_COUNT: usize = 20;
+
+fn fetch_images(start: usize, count: usize) -> Vec<Image> {
+    let mut images = Vec::new();
+    for i in start..start + count {
+        let image = Image {
+            src: format!("https://picsum.photos/200/300?random={}", i),
+            date: 
+                format!(
+                    "{}-{:02}-{:02}",
+                    rand::thread_rng().gen_range(2010..2022),
+                    rand::thread_rng().gen_range(1..13),
+                    rand::thread_rng().gen_range(1..29)
+                ),
+        };
+        images.push(image);
+    }
+    images
+}
+
+#[component]
+fn infinite_feed() -> impl IntoView {
+    let (images, wImages) = create_signal(Vec::new());
+    let (start, wStart) = create_signal(0);
+
+    let el = create_node_ref::<Div>();
+
+    let _ = use_infinite_scroll_with_options(
+        el,
+        move |_| async move {
+            let count = FETCH_IMAGE_COUNT; 
+            let newStart = start.get() + count;
+            let newImages = fetch_images(newStart, count);
+            wImages.update(|images| images.extend(newImages));
+            wStart.set(newStart);
+        },
+        UseInfiniteScrollOptions::default().distance(250.0),
+    );
+
+    wImages.set(fetch_images(start.get(), FETCH_IMAGE_COUNT));
+    view! {
+        <div
+            class="flowdiv"
+            node_ref=el
+            style="display: flex; flex-wrap: wrap; gap: 10px;"
+            >
+            <For each=move || images.get() key=|i| i.clone() let:image>
+                <div class="image">
+                    <img 
+                    src=image.src
+                    style=format!("height: {}px; width: {}px;", rand::thread_rng().gen_range(250..300), rand::thread_rng().gen_range(250..300))
+                    />
+                    <p>{image.date}</p>
+                </div>
+            </For>
+        </div>
+    }
+}
+
+
+
 #[component]
 fn TestPage() -> impl IntoView {
+    let el = create_node_ref::<Div>();
+
+    let (data, set_data) = create_signal(vec![1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7]);
+
+    let _ = use_infinite_scroll_with_options(
+        el,
+        move |_| async move {
+            let len = data.with_untracked(|d| d.len());
+            set_data.update(|data| *data = (1..len + 6).collect());
+        },
+        UseInfiniteScrollOptions::default().distance(10.0),
+    );
+
     view! {
-        <h1>"test"</h1>
+        <div
+          class="flowdiv"
+          node_ref=el
+        >
+            <ul>
+            <For each=move || data.get() key=|i| *i let:item>
+                <li>{item}</li>
+            </For>
+            </ul>
+        </div>
     }
 }
 
 #[component]
 fn HelloPage() -> impl IntoView {
+    
+    let (name, set_name) = create_signal("Potato".to_string());
+    
     view! {
         <h1>"Hello"</h1>
+        <input type="text"
+        on:input=move |ev| {
+            set_name(event_target_value(&ev));
+        }
+        prop:value=name
+        />
+        <p>"Name is: " {name}</p>
     }
 }
 
