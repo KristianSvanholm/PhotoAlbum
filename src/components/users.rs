3c8d091a9dd8673a2edb44cb6e29f4bfcab5e0be
kristian <kristianrorensvanholm@gmail.com>
2024-05-22 14:53:02 +0200

    Remove admin from search results

--- src/components/users.rs
+++ src/components/users.rs
@@ -17,9 +17,10 @@ pub async fn get_user_map() -> Result<Vec<UserInfo>, ServerFnError> {
 
     let pool = pool()?;
 
-    let users = sqlx::query_as::<_, UserInfo>("SELECT id, username FROM users")
-        .fetch_all(&pool)
-        .await?;
+    let users =
+        sqlx::query_as::<_, UserInfo>("SELECT id, username FROM users WHERE username != 'admin'")
+            .fetch_all(&pool)
+            .await?;
 
     Ok(users)
 }
